601a2e9b7ded7ff9b30936736f5f2486958167f6
Ashen Upendra Disanayaka <ashenupendra@live.com>
2020-10-04 13:35:03 +0800

    Accounting Page1

---
+++ cManager/Data/AccountService.cs
@@ -0,0 +1,50 @@
+﻿using cManager.Shared.Accounting;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cManager.Data
+{
+    public class AccountService
+    {
+        private readonly ApplicationDbContext dBContext;
+
+        public AccountService(ApplicationDbContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
+        public async Task<List<Account>> GetAccounts()
+        {
+            return await dBContext.Accounts.ToListAsync();
+        }
+
+        public Account GetAccount(int accountId)
+        {
+            return dBContext.Accounts.FirstOrDefault(e => e.Id == accountId);
+        }
+
+        public bool SaveAccount(Account account)
+        {
+            if (account.Id == 0)
+            {
+                dBContext.Add(account);
+            }
+            else
+            {
+                dBContext.Update(account);
+            }
+            dBContext.SaveChanges();
+            return true;
+        }
+
+        public bool DeleteAccount(int accountId)
+        {
+            var account = GetAccount(accountId);
+            account.IsActive = false;
+            return SaveAccount(account);
+        }
+    }
+}
--- cManager/Startup.cs
+++ cManager/Startup.cs
@@ -40,6 +40,7 @@ namespace cManager
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<AccountService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
